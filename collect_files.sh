2edf3a9536e58035f8dc63eaae8a1de11f83b04f
zp001paul <zp001@vip.tom.com>
2024-05-24 10:34:38 +0800

    2024-5-24 v2

--- collect_files.sh
+++ collect_files.sh
@@ -6,3 +6,7 @@ rsync -avlP ~/.vim .
 rsync -avlP ~/.config/kitty .
 rsync -avlP ~/.config/ranger .
 rsync -avlp ~/.oh-my-zsh/custom .
+
+
+mkdir containers
+cp /etc/containers/registries.conf ./containers/
